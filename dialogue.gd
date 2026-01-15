84fce557d22917080c4c976f886428adf55e1824
dalifyyy <d4lified@gmail.com>
2026-01-15 16:18:32 +0800

    .

--- dialogue.gd
+++ dialogue.gd
@@ -1,10 +1,27 @@
 extends Node
 
-var resource
+
+var dialog_scene := load("res://addons/dialogue_manager/example_balloon/example_balloon.tscn")
+var dres := load("res://dialogues/main.dialogue")
+var dialog_open := false
+var curr_dialog := ""
+
+func _ready():
+	DialogueManager.dialogue_ended.connect(dialog_end)
 
 func change_resource(path):
-	pass
+	dres = load(path)
 
-func talk(id):
+func dialog(id):
+	if not dialog_open:
+		var c = G.gn("dialogue_balloon")
+		if c == null:
+			var d = dialog_scene.instantiate()
+			G.gn("camera").get_node("CanvasLayer/dialogue_cover").add_child(d)
+		
+		G.gn("dialogue_balloon").start(dres, id)
+		dialog_open = true
+		curr_dialog = id
+
+func dialog_end(id):
 	pass
-	
--- global.gd
+++ global.gd
@@ -1,11 +1,4 @@
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func gm(): return get_tree().current_scene
+func gn(id): return get_tree().get_first_node_in_group(id)
